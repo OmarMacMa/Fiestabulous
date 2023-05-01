a8c7856c9fdc429df982ea654ca8d7cc78e3a734
Omar Macias <a01635795@tec.mx>
2023-04-30 20:18:31 -0600

    Still preparing the deploy

--- Backend/app/__init__.py
+++ Backend/app/__init__.py
@@ -10,10 +10,10 @@ def create_app():
     app = Flask(__name__)
     api = Api(app)
     mysql = MySQL(app)
-    app.config['MYSQL_HOST'] = os.environ.get('MYSQL_HOST')
-    app.config['MYSQL_USER'] = os.environ.get('MYSQL_USER')
-    app.config['MYSQL_PASSWORD'] = os.environ.get('MYSQL_PASSWORD')
-    app.config['MYSQL_DB'] = os.environ.get('MYSQL_DB')
+    app.config['MYSQL_HOST'] = MYSQL_HOST
+    app.config['MYSQL_USER'] = MYSQL_USER
+    app.config['MYSQL_PASSWORD'] = MYSQL_PASSWORD
+    app.config['MYSQL_DB'] = MYSQL_DB
     app.config["ssl_ca"] = "./DigiCertGlobalRootCA.crt.pem"
     # app.config["client_flags"] = [mysql.connector.ClientFlag.SSL]
     CORS(app)
